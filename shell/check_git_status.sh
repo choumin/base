2ae21e715f4304feb61424e36eb0cb9f53b6be74
zhoumin <zhoumin@loongson.cn>
2020-12-01 14:18:59 +0800

    update

--- shell/check_git_status.sh
+++ shell/check_git_status.sh
@@ -6,19 +6,29 @@ dir=$(cd $dir; pwd)
 echo "checking $dir ..."
 for elem in `ls $dir`
 do
-	dirty=false
+	msg1=""
+	msg2=""
+	msg3="clean"
 	elem=$dir/$elem
 	if [ -d "$elem" ]; then
 		a=$(cd $elem; git status |grep 'git checkout')
 		b=$(cd $elem; git status |grep 'git reset')
+		c=$(cd $elem; git status |grep 'git push')
 		if [ ! -z "$a" -o ! -z "$b" ]; then
-			dirty=true
+			msg1="uncommit"
+		fi
+		if [ ! -z "$c" ]; then
+			msg2="unpush"
 		fi
 	fi
 	#echo $dirty
-	if [ "$dirty" = true ]; then
-		echo "$elem : yes"
+	if [ ! -z "$msg1" -a ! -z "$msg2" ]; then
+		echo "$elem : $msg1, $msg2"
+	elif [ ! -z "$msg1" ]; then
+		echo "$elem : $msg1"
+	elif [ ! -z "$msg2" ]; then
+		echo "$elem : $msg2"
 	else
-		echo "$elem : no"
+		echo "$elem : $msg3"
 	fi
 done 
