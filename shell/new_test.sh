3d1247bd1cea5a6c49e58dcbff96f8c88391ee29
zhoumin <zhoumin@loongson.cn>
2021-01-21 16:15:21 +0800

    update

--- shell/new_test.sh
+++ shell/new_test.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+repo_base=/home/zhoumin/github/choumin/base
+src_dir=C/src
+
 function add_main() {
 	file="$1"
 	[ -z "$file" ] && "echo add main failed!" && return
@@ -37,19 +40,35 @@ clean:
 
 }
 
+function add_to_ignore() {
+	file="$1"
+	[ -z "$file" ] && return
+
+	ignore=$repo_base/.gitignore
+	[ ! -f $ignore ] && return
+
+	ret=`grep $file $ignore`
+	[ ! -z "$ret" ] && return
+
+	echo $file >> $ignore
+}
+
 if [ $# -eq 0 ]; then
-	if [ ! -f ./main.c ]; then
+	if [ ! -f main.c ]; then
 		add_main main.c
 	fi	
 
-	if [ ! -f ./Makefile ]; then
+	if [ ! -f Makefile ]; then
 		add_makefile Makefile
 
 	fi
+	_tmp=`pwd`
+	test_name=`basename $_tmp`
+	add_to_ignore $src_dir/$test_name/main
+
 else
 	test_name=$1
-	home=/home/zhoumin/github/choumin/base/C/src
-	test_dir=$home/$test_name
+	test_dir=$repo_base/$src_dir/$test_name
 	if [ -d $test_dir ]; then
 		echo "$test_dir already exists!"
 		exit -1
@@ -59,5 +78,6 @@ else
 	touch $test_dir/main.c
 	add_main $test_dir/main.c
 	add_makefile $test_dir/Makefile
+	add_to_ignore $src_dir/$test_name/main
 	echo "you can enter $test_name by j $test_dir"
 fi
